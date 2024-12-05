0d7b655d55ad01b418e9ad90c4515c69b17255b8
Rajkumar Jangam <rajkumarJangam66@gmail.com>
2024-12-05 14:58:58 +0530

    rate limiting added to weather-by-ip api end point

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UtilsModule } from './utils/utils.module';
 import { ThirdPartyModule } from './third-party/third-party.module';
 import { WeatherReportModule } from './weather-report/weather-report.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 0, limit: 0 }]),
     UtilsModule,
     WeatherReportModule,
     ThirdPartyModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/weather-report/weather-report.controller.ts
+++ src/weather-report/weather-report.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { WeatherReportService } from './weather-report.service';
-
+import { Throttle } from '@nestjs/throttler';
 @Controller('weather-report')
 export class WeatherReportController {
   constructor(private readonly weatherReportService: WeatherReportService) {}
 
   @Get('/weather-by-ip')
+  @Throttle({ default: { limit: 5, ttl: 5 * 60 } })
   async getWeatherByIp(@Query('ip') ip: string) {
     const res = await this.weatherReportService.getWeatherByIp(ip);
     return res;
